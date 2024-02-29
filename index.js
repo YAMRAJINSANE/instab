3f37ae6e58146520ff71eb51947cbcb8346e201f
akash <akashjnv007@gmail.com>
2024-02-29 15:05:17 +0530

    gt

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 require("dotenv").config();
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
-
-const { IgApiClient } = require('instagram-private-api');
-const { get } = require('request-promise');
+const fs = require("fs");
+const { IgApiClient } = require("instagram-private-api");
 const CronJob = require("cron").CronJob;
 
-const postToInsta = async () => {
-    const ig = new IgApiClient();
-    ig.state.generateDevice(process.env.IG_USERNAME);
-    await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+(async () => {
+  const ig = new IgApiClient();
+  ig.state.generateDevice(process.env.IG_USERNAME);
+  ig.state.proxyUrl = process.env.IG_PROXY;
+  const auth = await ig.account.login(
+    process.env.IG_USERNAME,
+    process.env.IG_PASSWORD
+  );
+  console.log(JSON.stringify(auth));
 
-    const imageBuffer = await get({
-        url: 'https://i.imgur.com/BZBHsauh.jpg',
-        encoding: null, 
-    });
+  // Upload multiple local images
+  const imagePaths = [
+    "C:/Users/mkuma/Downloads/ImgDownVoccab/img30.jpeg",
+    "C:/Users/mkuma/Downloads/ImgDownVoccab/img40.jpeg",
+  ];
 
-    await ig.publish.photo({
-        file: imageBuffer,
-        caption: 'Really nice photo from the internet!',
-    });
-}
 
-const cronInsta = new CronJob("30 5 * * *", async () => {
-    postToInsta();
-});
+  const imageBuffers = await Promise.all(
+    imagePaths.map((path) => fs.promises.readFile(path))
+  );
 
-cronInsta.start();
+  const albumItems = imageBuffers.map((buffer) => ({
+    file: buffer,
+    // You can optionally add usertags here
+  }));
+
+  const publishResult = await ig.publish.album({
+    items: albumItems, // Array of objects, each representing an image in the album
+    caption: `sscenglish2024 sscenglish2024 Top Voccab 📕📕  words asked IN #ssc #exam
+
+#ssc #ssccgl
+
+#sscexam #sscenglish #englishexam #englishiseasy #sscchslexam #englishlanguageteaching #englishlearner #examday #englishtown #englishmastiffpuppy #englishquiz #sscycle #englishgrammer #englisheveryday #sscgk #englishacademy #englishman #englishtoyterrier #ssccglcpocds #examsover #sscexams #englishword #cglove`, // Caption for the album (optional)
+  });
+
+  console.log(publishResult); // publishResult.status should be "ok"
+})();
